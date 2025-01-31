4eb190a432dd5293e2ef41c132429752bd3ac4df
yupashchenko01 <yu.pashchenko1@gmail.com>
2025-01-31 03:14:33 +0200

    functions.h

---
+++ functions.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS1_H_
+
+void disease_add();
+void medication_add();
+void diseases_display();
+void medications_display();
+void disease_delete();
+void medication_delete();
+void disease_find();
+void medications_find();
+void adjust_quantity();
+void disease_sort();
+void medications_sort();
+void diseases_read();
+void medications_read();
+void diseases_save();
+void medications_save();
+
+#endif
